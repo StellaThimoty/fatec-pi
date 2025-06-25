8e0614abb0a8975528d82085e0cd70e77346cc53
Murilo Gruthner <134981137+murilogruthner@users.noreply.github.com>
2025-06-24 23:59:30 -0300

    Add files via upload

--- app/Http/Controllers/CardsController.php
+++ app/Http/Controllers/CardsController.php
@@ -27,30 +27,30 @@ class CardsController extends Controller
     }
 
     public function update(Request $request, Cards $card) {
-        $request->validate([
-        'game' => 'required|string',
-        'name' => 'required|string',
-        'price' => 'required|decimal:0,2',
-        'setNumber' => 'required|string',
-        'rarity' => 'required|string',
-        'state' => 'required|string',
-        'altart' => 'nullable|bool',
-        'color' => 'nullable|string',
-        'quantity' => 'required|numeric',
-        'observations' => 'nullable|string'
-        ]);
+        if($request->hasFile('pics')){
+            $file = $request->file('pics');
+            $extension = $file->getClientOriginalExtension();
+
+            $filename = time()+'.'.$extension;
+            $path = 'uploads/cards/';
+            $file->move($path,$filename);
+        } else {
+            $path = '';
+            $filename = '';
+        }
 
         $card->update([
-            'game' => $request->input('game'),
-            'name' => $request->input('name'),
-            'price' => $request->input('price'),
-            'setNumber' => $request->input('setNumber'),
-            'rarity' => $request->input('rarity'),
-            'state' => $request->input('state'),
-            'altart' =>$request->input('altart'),   
-            'color' => $request->input('color'),
-            'quantity' => $request->input('quantity'),
-            'observations' => $request->input('observations'),
+            'game' => $request->get('game', $card->game),
+            'name' => $request->get('name', $card->name),
+            'price' => $request->get('price', $card->price),
+            'setNumber' => $request->get('setNumber', $card->setNumber),
+            'rarity' => $request->get('rarity', $card->rarity),
+            'state' => $request->get('state', $card->state),
+            'altart' =>$request->get('altart', $card->altart),   
+            'color' => $request->get('color', $card->color),
+            'quantity' => $request->get('quantity', $card->quantity),
+            'image' => $path.$filename,
+            'observations' => $request->get('observations', $card->observations),
         ]);
 
         return redirect()->route('cards.cards')->with('message', 'Carta editada com sucesso');
@@ -70,7 +70,16 @@ class CardsController extends Controller
         'observations' => 'nullable|string'
         ]);
         $id = Auth::id();
+        if($request->hasFile('pics')){
+            $file = $request->file('pics');
 
+            $filename = $file->getClientOriginalName();
+            $path = 'uploads/cards/';
+            $file->move($path,$filename);
+        } else {
+            $path = '';
+            $filename = '';
+        }
 
         Cards::create([
             'userId' => $id,
@@ -83,6 +92,7 @@ class CardsController extends Controller
             'altart' =>$request->input('altart'),   
             'color' => $request->input('color'),
             'quantity' => $request->input('quantity'),
+            'image' => $path.$filename,
             'observations' => $request->input('observations'),
         ]);
 
